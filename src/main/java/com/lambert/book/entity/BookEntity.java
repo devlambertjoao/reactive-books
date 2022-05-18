105aeb609fa9f7ab2709c394fd1ccba86e6a3284
Joao Lambert <joao.lambert@gft.com>
2022-05-17 21:39:12 -0300

    Configuring database

--- src/main/java/com/lambert/book/entity/BookEntity.java
+++ src/main/java/com/lambert/book/entity/BookEntity.java
@@ -1,5 +1,49 @@
 package com.lambert.book.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Entity
+@Table("TB_BOOK")
 public class BookEntity {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "BOOK_ID")
+	private Long id;
+
+	@Column(name = "BOOK_TITLE")
+	private String title;
+
+	@Column(name = "BOOK_AUTHOR_NAME")
+	private String authorName;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthorName() {
+		return authorName;
+	}
+
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
 }
